356ffecdeb6fd7a5b6e384d8f1abbe20f0fff42a
Jigar Sony <sonijigar94@gmail.com>
2026-05-01 19:11:02 +0530

    paramAndDynamic

--- ReadMe.md
+++ ReadMe.md
@@ -584,6 +584,7 @@ expect(await page.screenshot()).toMatchSnapshot('visualTest.png');
 ```
 
 File
+
 ```javascript
 const {test, expect} = require('@playwright/test');
 
@@ -659,4 +660,42 @@ async function excelTest() {
 }
     excelTest();
 // how to run this - locate till this folder - node <fileName>
+```
+
+## lect#77 - Wrap the code into func
+
+```javascript
+const ExcelJs = require('exceljs');
+
+async function writeExcelTest(searchText, replaceText, change, filePath) {
+    const workbook = new ExcelJs.Workbook();
+    // https://rahulshettyacademy.com/upload-download-test/index.html
+    await workbook.xlsx.readFile(filePath);
+    const worksheet = workbook.getWorksheet('Sheet1');
+    const output = await readExcel(worksheet, searchText);
+    // replace cell value in excel
+    const cell = worksheet.getCell(output.row + change.rowChange, output.column + change.colChange);
+    cell.value = replaceText;
+    await workbook.xlsx.writeFile("/Users/jigarsony/Downloads/excelDownloadTest.xlsx");
+}
+
+async function readExcel(worksheet, searchText) {
+    let output = { row: -1, column: -1 };
+    //get all or specific text
+    worksheet.eachRow((row, rowNumber) => {
+        row.eachCell((cell, colNumber) => {
+            //console.log(cell.value) //- print all ExcelSheet
+            if (cell.value === searchText) {
+                // console.log(rowNumber);
+                // console.log(colNumber);
+                output.row = rowNumber;
+                output.column = colNumber;
+            }
+        })
+    })
+    return output;
+}
+writeExcelTest("Mango", 350, { rowChange: 0, colChange: 2 }, "/Users/jigarsony/Downloads/excelDownloadTest.xlsx");
+// how to run this - locate till this folder - node <fileName>
+// update mango proce to 250
 ```
--- tests/excelJSUtil/excelDemo.js
+++ tests/excelJSUtil/excelDemo.js
@@ -1,30 +1,33 @@
 const ExcelJs = require('exceljs');
 
-async function excelTest() {
-    let output = {row:-1,column:-1};
+async function writeExcelTest(searchText, replaceText, change, filePath) {
     const workbook = new ExcelJs.Workbook();
     // https://rahulshettyacademy.com/upload-download-test/index.html
-    await workbook.xlsx.readFile("/Users/jigarsony/Downloads/excelDownloadTest.xlsx");
-
+    await workbook.xlsx.readFile(filePath);
     const worksheet = workbook.getWorksheet('Sheet1');
+    const output = await readExcel(worksheet, searchText);
+    // replace cell value in excel
+    const cell = worksheet.getCell(output.row + change.rowChange, output.column + change.colChange);
+    cell.value = replaceText;
+    await workbook.xlsx.writeFile("/Users/jigarsony/Downloads/excelDownloadTest.xlsx");
+}
 
+async function readExcel(worksheet, searchText) {
+    let output = { row: -1, column: -1 };
     //get all or specific text
     worksheet.eachRow((row, rowNumber) => {
         row.eachCell((cell, colNumber) => {
             //console.log(cell.value) //- print all ExcelSheet
-            if (cell.value === "Banana") {
+            if (cell.value === searchText) {
                 // console.log(rowNumber);
                 // console.log(colNumber);
-                output.row=rowNumber;
-                output.column=colNumber;
+                output.row = rowNumber;
+                output.column = colNumber;
             }
         })
     })
-
-    // replace cell value in excel
-    const cell = worksheet.getCell(output.row,output.column);
-    cell.value = "Republic";
-    await workbook.xlsx.writeFile("/Users/jigarsony/Downloads/excelDownloadTest.xlsx");
+    return output;
 }
-    excelTest();
+writeExcelTest("Mango", 350, { rowChange: 0, colChange: 2 }, "/Users/jigarsony/Downloads/excelDownloadTest.xlsx");
 // how to run this - locate till this folder - node <fileName>
+// update mango proce to 250
